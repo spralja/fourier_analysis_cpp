395a3c80caf99eda1eb4b1e03badeab1bf30c037
Kyle <kyle@weld.app>
2021-12-12 01:51:04 +0100

    Unfucked the delete

---
+++ Coefficient.h
@@ -0,0 +1,26 @@
+//
+// Created by Robert on 07/12/2021.
+//
+
+#ifndef FOURIER_ANALYSIS_CPP_COEFFICIENT_H
+#define FOURIER_ANALYSIS_CPP_COEFFICIENT_H
+
+
+#include <utility>
+
+class FourierAnalysis;
+
+class Coefficient {
+public:
+    const double F;
+    const double G;
+
+    explicit Coefficient(const double& F,const double& G);
+
+    Coefficient(const std::pair<double, double>& that);
+
+    Coefficient conjugate() const;
+};
+
+
+#endif //FOURIER_ANALYSIS_CPP_COEFFICIENT_H
